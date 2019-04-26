4905dec18d3855b45c44b0ece7849f87ab828f47
jenmyers <jen.myers@flatironschool.com>
2019-04-26 10:46:36 -0500

    Add new test solution

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,6 +33,7 @@ end
 def retrieve_last_element_from_array(array)
   array.last  
 end
-def update_element_from_index(array, index_number, element)
 
+def update_element_from_index(array, index_number, element)
+  array[index_number] = element
 end
