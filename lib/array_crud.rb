e514f975f5bdeed8f15c648b64e20f438dbcdbd0
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-27 20:00:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.shift("wow")
+  p array
 end
 
 def remove_element_from_end_of_array(array)
