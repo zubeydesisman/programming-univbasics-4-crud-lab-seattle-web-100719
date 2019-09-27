dc2baab0324ff692130766d71a3ba1baf6d47ab4
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-27 19:58:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array << 5
-  p my_array
+  array << 5
+  p array
 end
 
 def add_element_to_start_of_array(array, element)
